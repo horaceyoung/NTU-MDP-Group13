112c84fcf4faccc9b77ddce3bb01b0bca2efa61b
HORACEYOUNG <32321396+HORACEYOUNG@users.noreply.github.com>
2020-09-08 19:59:34 +0800

    Revert "Revert "Updated""
    This reverts commit 8bf063a3b3c317f5df714081c7b5bfc08425799f.

--- Simulator/simulatorMain.py
+++ Simulator/simulatorMain.py
@@ -3,10 +3,16 @@ from settings import *
 import pygame as pg
 import map_generator
 import robot
+import configurations
+import map
+import exploration
 
 pg.init()
 screen = pg.display.set_mode((1024, 768))
 pg.display.set_caption("MDP Arena Simulator")
+
+
+
 
 player_robot = robot.Robot()
 robot_group = pg.sprite.Group(player_robot)
@@ -16,6 +22,16 @@ for tile in arena_map.tiles.sprites():
     print(tile.rect.x, tile.rect.y)
 
 running = True
+
+
+#Testing##############################################################
+test_robot = robot.RobotAlgo(configurations.START_ROW,configurations.START_COL,False,player_robot)
+exploredMap = map.Map(test_robot)
+realMap = map.Map(test_robot)
+exploredMap.setAllUnexplored()
+explorer = exploration.Exploration(exploredMap,realMap,test_robot,300,5/100000)
+#######################################################################
+
 
 while running:
     screen.fill((0,0,0))
@@ -30,11 +46,10 @@ while running:
             running = False
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_w:
-                player_robot.move_forward()
+                explorer.runExploration()
             if event.key == pg.K_a:
                 player_robot.rotate(-90)
             if event.key == pg.K_d:
                 player_robot.rotate(90)
     pg.sprite.groupcollide(player_robot.censors, arena_map.tiles, False, True)
     pg.display.update()
-
