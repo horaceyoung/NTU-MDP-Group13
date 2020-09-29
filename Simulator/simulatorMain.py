14a54b2885e5ba937954b2511785725a2ac59156
wakanda4evar <wakanda4evarr@gmail.com>
2020-09-29 11:12:43 +0800

    Updated

--- Simulator/simulatorMain.py
+++ Simulator/simulatorMain.py
@@ -3,7 +3,7 @@ import fastest_path
 import exploration
 import map
 import robot
-import testComm
+import commMgr
 
 from configurations import *
 
@@ -18,15 +18,15 @@ robot_group = pg.sprite.Group(player_robot)
 arena_map = map.Map()
 arena_map.generate_map('map_config_2.txt')
 realRun = False
-exploration_instance = exploration.Exploration(250, 100, player_robot, arena_map, False)
+exploration_instance = exploration.Exploration(300, 20, player_robot, arena_map, False)
 #exploration_instance.initialize_exploration()
 # an unresolved issue
 arena_map.map_cells[18][3].discovered=True
 arena_map.map_cells[19][3].discovered=True
 
 running = True
-#comm = testComm.TcpClient("127.0.0.1", 22, buffer_size=1024) #For debugging purpose
-#testcom = testComm.TcpClient("192.168.13.13", 22, buffer_size=1024)
+#comm = commMgr.TcpClient("127.0.0.1", 22, buffer_size=1024) #For debugging purpose
+#testcom = commMgr.TcpClient("192.168.13.13", 22, buffer_size=1024)
 #comm.run()
 
 while running:
@@ -48,13 +48,12 @@ while running:
             #(Added) Test Real Run ####################################################################
             if event.key == pg.K_r:
                 realRun = True
-                comm = testComm.TcpClient("127.0.0.1", 22, buffer_size=1024) #For debugging purpose
-                #comm = testComm.TcpClient("192.168.13.13", 22, buffer_size=1024)
+                #comm = commMgr.TcpClient("127.0.0.1", 22, buffer_size=1024) #For debugging purpose
+                comm = commMgr.TcpClient("192.168.13.13", 22, buffer_size=2048)
                 comm.run()
-                exploration_instance = exploration.Exploration(298, 100, player_robot, arena_map, True, comm)
+                exploration_instance = exploration.Exploration(300, 100, player_robot, arena_map, True, comm)
                 exploration_instance.initialize_exploration()
-            if event.key == pg.K_m:
-                arena_map.generate_descriptor_strings()
+
 
 
     for sensor in player_robot.sensors:
@@ -65,7 +64,7 @@ while running:
         exploration_instance.exploration_loop()
     #fastest_path.astar(arena_map,player_robot.location,(18,1))
 
-    clock.tick(20)
+    clock.tick(2)
 
     screen.fill((0, 0, 0))
     # generate the map
